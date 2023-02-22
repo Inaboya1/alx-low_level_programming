b07abdf4567210a35043395623bcfb7edeb8587e
Inaboya1 <inaboya1@gmail.com>
2023-02-21 17:37:20 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,6 +15,6 @@ printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
 printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of a long int: %lu bytes(s)\n", (unsigned long)sizeof(c));
 printf("Size of a long long: %lu bytes(s)\n", (unsigned long)sizeof(d));
-printf("Size of a float: %lu bytes\n", (unsigned long)sizeof(f));
+printf("Size of a float: %lu bytes(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
